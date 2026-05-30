ecaba075a7b5abd3334cffc43bcb79398c67aa24
Noiker-UX <pasha_pasha_201@mail.ru>
2026-05-30 17:53:35 +0300

    create action delete and update

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
   Res,
 } from '@nestjs/common';
@@ -15,6 +17,7 @@ export class CoffeesController {
   @Get('/all')
   findAll(@Res() responce: express.Response) {
     responce.status(200).send('this call all coffees');
+    responce.status(404).send('ups');
   }
 
   @Get(':id')
@@ -26,5 +29,18 @@ export class CoffeesController {
   @HttpCode(HttpStatus.GONE)
   create(@Body() body: { name: string; price: string }) {
     return body;
+  }
+
+  @Delete('/delete/:id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() body: { name: string; price: number },
+  ) {
+    return `This action updates #${id} coffee with body ${JSON.stringify(body)}`;
   }
 }
